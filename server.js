de07a36a55f3638bccfe99dc595d2b5bc0942308
bryan.gonzalez96.bg@gmail.com <bryan.gonzalez96.bg@gmail.com>
2018-08-02 12:56:45 -0500

    Version 2.0

--- server.js
+++ server.js
@@ -20,6 +20,13 @@ mongoose
 app.use('/api/apartments', apartment);
 app.use('/api/houses', houses);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const port = process.env.PORT || 5000;
 
